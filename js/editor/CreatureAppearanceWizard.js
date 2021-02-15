23a5e584223b9a0e608d01ec3885d8ea4c3020cf
KobaltBlu <KobaltBlu@users.noreply.github.com>
2021-02-15 10:31:16 -0500

    Update CreatureAppearanceWizard.js

--- js/editor/CreatureAppearanceWizard.js
+++ js/editor/CreatureAppearanceWizard.js
@@ -50,7 +50,6 @@ class CreatureAppearanceWizard extends Wizard {
         let appearanceData = Global.kotor2DA['appearance'].rows[i];
 
         $appearanceBlock.addClass('noselect').addClass('model-thumbnail').attr('tabindex', 1);
-
         $appearanceBlock.appId = appearanceData['(Row Label)'];
         $appearanceBlock.isRendered = false;
 
@@ -82,9 +81,9 @@ class CreatureAppearanceWizard extends Wizard {
 
   }
 
-  Update(){
+  async Update(){
     if(!this._destroyed){
-      this.RenderVisibleQueue();
+      await this.RenderVisibleQueue();
 
       requestAnimationFrame( () => {
         this.Update();
@@ -93,23 +92,20 @@ class CreatureAppearanceWizard extends Wizard {
   }
 
   GetCellDimensions(){
-
     let maxWidth = (this.$list.width() / 4);
-
     return {width: maxWidth, height: maxWidth * 2};
-
   }
 
   IsOnscreen(el){
     let rect = el.getBoundingClientRect();
     return (
-           (rect.x + rect.width) < 0
-             || (rect.y + rect.height) < 0
-             || (rect.x > window.innerWidth || rect.y > window.innerHeight)
-         );
+      (rect.x + rect.width) < 0
+        || (rect.y + rect.height) < 0
+        || (rect.x > window.innerWidth || rect.y > window.innerHeight)
+    );
   }
 
-  RenderVisibleQueue(index = 0){
+  async RenderVisibleQueue(index = 0){
 
     let $item = this.queue[index];
 
@@ -119,45 +115,45 @@ class CreatureAppearanceWizard extends Wizard {
     if(typeof $item != 'undefined'){
       if($item.isVisible() && !$item.isRendered){
         $item.isRendered = true;
-        this.RenderView($item, () => {
-          this.queue[index] = undefined;
-          this.RenderVisibleQueue(index+=1);
-        });
+        await this.RenderView($item);
+        this.queue[index] = undefined;
+        this.RenderVisibleQueue(++index);
       }else{
-        this.RenderVisibleQueue(index+=1);
+        this.RenderVisibleQueue(++index);
       }
-
     }else{
-      this.RenderVisibleQueue(index+=1);
+      this.RenderVisibleQueue(++index);
     }
 
   }
 
-  RenderView($element, onRender = null){
+  RenderView($element){
+    return new Promise( (resolve, reject ) => {
 
-    let appearanceLoader = new AppearanceLoader({
-      id: $element.appId
+      let appearanceLoader = new AppearanceLoader({
+        id: $element.appId,
+        context: this.ui3DRenderer
+      });
+
+      appearanceLoader.GetModel( (model) => {
+        let scene = this.ui3DRenderer.ResetScene();
+        scene.add(model);
+
+        let center = model.box.getCenter();
+        if(!isNaN(center.length())){
+          model.position.set(-center.x, -center.y, -center.z);
+        }else{
+          model.position.set(0, 0, 0);
+        }
+
+        this.ui3DRenderer.Render();
+        $('img', $element).attr('src', this.ui3DRenderer.GetRenderedImage()).css('visibility', 'visible');
+        resolve();
+      }, (e) => {
+        resolve();
+      });
+
     });
-
-    appearanceLoader.GetModel( (model) => {
-      //let dims = this.GetCellDimensions();
-      //this.ui3DRenderer.SetSize(dims.width, dims.height);
-
-      let scene = this.ui3DRenderer.ResetScene();
-      scene.add(model);
-
-      this.ui3DRenderer.Render();
-      $('img', $element).attr('src', this.ui3DRenderer.GetRenderedImage()).css('visibility', 'visible');
-
-      if(onRender != null && typeof onRender === 'function')
-        onRender();
-
-    }, (e) => {
-      console.error(e);
-      if(onRender != null && typeof onRender === 'function')
-        onRender();
-    });
-
   }
 
 
