2c7cd27de80ca6f8cc9ab01899a9919c4d877398
KobaltBlu <KobaltBlu@users.noreply.github.com>
2021-07-18 09:00:45 -0400

    Update Utility.js

--- js/Utility.js
+++ js/Utility.js
@@ -203,6 +203,18 @@ class Utility {
 		return Math.abs(Math.sqrt( dx * dx + dy * dy ));
   }
 
+  static LineLineIntersection (a,b,c,d,p,q,r,s) {
+    var det, gamma, lambda;
+    det = (c - a) * (s - q) - (r - p) * (d - b);
+    if (det === 0) {
+      return false;
+    } else {
+      lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
+      gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
+      return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+    }
+  };
+
 
 }
 
