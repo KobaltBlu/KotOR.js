0b4a8c53941ca7a50102b1d403c458e1c863725e
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2021-12-03 10:58:11 -0500

    update: EventSignalEvent save/load logic

--- js/events/EventSignalEvent.js
+++ js/events/EventSignalEvent.js
@@ -6,11 +6,19 @@ class EventSignalEvent extends GameEvent {
     //Event Type
     this.type = GameEvent.Type.EventSignalEvent;
 
+    this.event = undefined;
+
+  }
+
+  setEvent(event = undefined){
+    if(event instanceof NWScriptEvent){
+      this.event = event;
+    }
   }
 
   eventDataFromStruct(struct){
     if(struct instanceof Struct){
-      
+      this.event = NWScriptEvent.EventFromStruct(struct);
     }
   }
 
@@ -24,7 +32,11 @@ class EventSignalEvent extends GameEvent {
     struct.AddField( new Field(GFFDataTypes.DWORD, 'CallerId') ).SetValue( this.script.caller instanceof ModuleObject ? this.script.caller.id : 2130706432 );
     struct.AddField( new Field(GFFDataTypes.DWORD, 'Day') ).SetValue(this.day);
     let eventData = struct.AddField( new Field(GFFDataTypes.STRUCT, 'EventData') );
-    //eventData.AddChildStruct( this.script.saveEventSituation() );
+    if(this.event instanceof NWScriptEvent){
+      let eStruct = this.event.save();
+      eStruct.SetType(0x4444);
+      eventData.AddChildStruct( eStruct );
+    }
     struct.AddField( new Field(GFFDataTypes.DWORD, 'EventId') ).SetValue(this.id);
     struct.AddField( new Field(GFFDataTypes.DWORD, 'ObjectId') ).SetValue( this.script.object instanceof ModuleObject ? this.script.object.id : 2130706432 );
     struct.AddField( new Field(GFFDataTypes.DWORD, 'Time') ).SetValue(this.time);
