c3f20d1a978d2cf62725c499a4208cf5f056ad46
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2022-03-04 08:09:33 -0500

    Update EventBroadcastAOO.js

--- js/events/EventBroadcastAOO.js
+++ js/events/EventBroadcastAOO.js
@@ -5,17 +5,24 @@ class EventBroadcastAOO extends GameEvent {
 
     //Event Type
     this.type = GameEvent.Type.EventBroadcastAOO;
+    this.value = 0;
 
   }
 
   eventDataFromStruct(struct){
     if(struct instanceof Struct){
-      
+      this.value = struct.GetFieldByLabel('Value').GetValue();
     }
   }
 
   execute(){
     
+  }
+
+  saveEventData(){
+    let struct = new Struct(0x9999);
+    struct.AddField( new Field(GFFDataTypes.DWORD, 'Value' ) ).SetValue(this.value);
+    return struct;
   }
 
   export(){
@@ -24,7 +31,7 @@ class EventBroadcastAOO extends GameEvent {
     struct.AddField( new Field(GFFDataTypes.DWORD, 'CallerId') ).SetValue( this.script.caller instanceof ModuleObject ? this.script.caller.id : 2130706432 );
     struct.AddField( new Field(GFFDataTypes.DWORD, 'Day') ).SetValue(this.day);
     let eventData = struct.AddField( new Field(GFFDataTypes.STRUCT, 'EventData') );
-    //eventData.AddChildStruct( this.script.saveEventSituation() );
+        eventData.AddChildStruct( this.saveEventData() );
     struct.AddField( new Field(GFFDataTypes.DWORD, 'EventId') ).SetValue(this.id);
     struct.AddField( new Field(GFFDataTypes.DWORD, 'ObjectId') ).SetValue( this.script.object instanceof ModuleObject ? this.script.object.id : 2130706432 );
     struct.AddField( new Field(GFFDataTypes.DWORD, 'Time') ).SetValue(this.time);
