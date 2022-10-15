2e8b7ee729602a1df0e2eaeb0e8950fe4d508f77
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2022-10-15 02:09:05 -0400

    add: ConfigClient stub

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,18 @@
 export {};
 
+interface ConfigClient {
+  get(key: string): any
+  set(key: string, value: any): any,
+  getRecentProjects(): any[],
+  getRecentFiles(): any[]
+}
+
 declare global {
   interface String {
     titleCase() : string;
     equalsIgnoreCase(value: string): boolean;
   }
-
+  interface Window { 
+    ConfigClient: ConfigClient
+  }
 }
