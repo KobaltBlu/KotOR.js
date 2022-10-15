c7adc941554128b46543c2137683ee6810924a4b
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2022-10-15 02:08:39 -0400

    add: dice

---
+++ src/utility/Dice.ts
@@ -0,0 +1,39 @@
+export class Dice {
+
+  static rollD2(num: number = 0){
+    
+  }
+  
+  static rollD3(num: number = 0){
+    
+  }
+  
+  static rollD4(num: number = 0){
+    
+  }
+  
+  static rollD6(num: number = 0){
+    
+  }
+  
+  static rollD8(num: number = 0){
+    
+  }
+  
+  static rollD10(num: number = 0){
+    
+  }
+  
+  static rollD12(num: number = 0){
+    
+  }
+  
+  static rollD20(num: number = 0){
+    
+  }
+  
+  static rollD100(num: number = 0){
+    
+  }
+
+}
