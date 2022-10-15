06f7237fa567536ae62129a9caa083a74bb2f7b6
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2022-10-15 02:06:10 -0400

    add: shader manager

---
+++ src/managers/ShaderManager.ts
@@ -0,0 +1,27 @@
+import * as THREE from "three";
+import { Shader } from "../shaders/Shader";
+import { ShaderOdysseyEmitter } from "../shaders/ShaderOdysseyEmitter";
+import { ShaderAuroraGUI } from "../shaders/ShaderAuroraGUI";
+import { ShaderOdysseyModel } from "../shaders/ShaderOdysseyModel";
+import { ShaderGrass } from "../shaders/ShaderGrass";
+
+export class ShaderManager {
+
+  static Shaders: Map<string, Shader> = new Map();
+
+  static AddShader(shader: Shader){
+    THREE.ShaderLib[shader.name] = {
+      fragmentShader: shader.getFragment(),
+      vertexShader: shader.getVertex(),
+      uniforms: THREE.UniformsUtils.merge(shader.getUniforms())
+    };
+    ShaderManager.Shaders.set(shader.name, shader);
+  }
+  
+
+}
+
+ShaderManager.AddShader(new ShaderGrass());
+ShaderManager.AddShader(new ShaderOdysseyModel());
+ShaderManager.AddShader(new ShaderOdysseyEmitter());
+ShaderManager.AddShader(new ShaderAuroraGUI());
