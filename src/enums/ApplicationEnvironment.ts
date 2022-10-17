ea32907ca9ecbf5a2fdf0298110f6ca9f5007528
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2022-10-17 18:25:55 -0400

    update: ApplicationProfile

---
+++ src/enums/ApplicationEnvironment.ts
@@ -0,0 +1,5 @@
+
+export enum ApplicationEnvironment {
+  BROWSER = "BROWSER",
+  ELECTRON = "ELECTRON",
+}
--- src/utility/ApplicationProfile.ts
+++ src/utility/ApplicationProfile.ts
@@ -1,5 +1,10 @@
+import { ApplicationEnvironment } from "../enums/ApplicationEnvironment";
+import { ApplicationMode } from "../enums/ApplicationMode";
+
 export class ApplicationProfile {
 
+  static MODE: ApplicationMode;
+  static ENV: ApplicationEnvironment;
   static directory: string;
   static key: string;
   static launch: any;
