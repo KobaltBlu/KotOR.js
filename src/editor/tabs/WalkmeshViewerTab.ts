4229ddf0923befa362f0b7f4e4a324f04f61c94e
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2022-10-19 21:02:59 -0400

    fix: ignore compile errors

--- src/editor/tabs/WalkmeshViewerTab.ts
+++ src/editor/tabs/WalkmeshViewerTab.ts
@@ -148,6 +148,7 @@ export class WalkmeshViewerTab extends EditorTab {
 
     this.$ui_selected = $('<div style="position: absolute; top: 0; right: 0; bottom: 0;" />');
 
+    //@ts-expect-error
     this.$ui_selected.windowPane({
       title: 'Walkmesh Viewer Tools'
     });
@@ -166,6 +167,7 @@ export class WalkmeshViewerTab extends EditorTab {
       }
     });
 
+    //@ts-expect-error
     $('#container').layout({ applyDefaultStyles: false,
       onresize: () => {
         try{
