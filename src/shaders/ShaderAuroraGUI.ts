3530bd07dd525b4595cb644ccb4520ea69ea0afc
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2023-02-06 18:32:49 -0500

    fix: gui shader

--- src/shaders/ShaderAuroraGUI.ts
+++ src/shaders/ShaderAuroraGUI.ts
@@ -19,11 +19,11 @@ export class ShaderAuroraGUI extends Shader {
     #include <uv2_pars_fragment>
     #include <map_pars_fragment>
     #include <alphamap_pars_fragment>
+    #include <alphatest_pars_fragment>
     #include <aomap_pars_fragment>
     #include <lightmap_pars_fragment>
     #include <envmap_common_pars_fragment>
     #include <envmap_pars_fragment>
-    #include <cube_uv_reflection_fragment>
     #include <fog_pars_fragment>
     #include <specularmap_pars_fragment>
     #include <logdepthbuf_pars_fragment>
@@ -39,9 +39,8 @@ export class ShaderAuroraGUI extends Shader {
       #include <specularmap_fragment>
       ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
       #ifdef USE_LIGHTMAP
-      
-        vec4 lightMapTexel= texture2D( lightMap, vUv2 );
-        reflectedLight.indirectDiffuse += lightMapTexelToLinear( lightMapTexel ).rgb * lightMapIntensity;
+        vec4 lightMapTexel = texture2D( lightMap, vUv2 );
+        reflectedLight.indirectDiffuse += lightMapTexel.rgb * lightMapIntensity * RECIPROCAL_PI;
       #else
         reflectedLight.indirectDiffuse += vec3( 1.0 );
       #endif
@@ -49,7 +48,7 @@ export class ShaderAuroraGUI extends Shader {
       reflectedLight.indirectDiffuse *= diffuseColor.rgb;
       vec3 outgoingLight = reflectedLight.indirectDiffuse;
       #include <envmap_fragment>
-      gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+      #include <output_fragment>
       #include <tonemapping_fragment>
       #include <encodings_fragment>
       #include <fog_fragment>
@@ -72,20 +71,21 @@ export class ShaderAuroraGUI extends Shader {
       #include <uv_vertex>
       #include <uv2_vertex>
       #include <color_vertex>
-      #include <skinbase_vertex>
-      #ifdef USE_ENVMAP
-      #include <beginnormal_vertex>
-      #include <morphnormal_vertex>
-      #include <skinnormal_vertex>
-      #include <defaultnormal_vertex>
+      #include <morphcolor_vertex>
+      #if defined ( USE_ENVMAP ) || defined ( USE_SKINNING )
+        #include <beginnormal_vertex>
+        #include <morphnormal_vertex>
+        #include <skinbase_vertex>
+        #include <skinnormal_vertex>
+        #include <defaultnormal_vertex>
       #endif
       #include <begin_vertex>
       #include <morphtarget_vertex>
       #include <skinning_vertex>
       #include <project_vertex>
       #include <logdepthbuf_vertex>
-      #include <worldpos_vertex>
       #include <clipping_planes_vertex>
+      #include <worldpos_vertex>
       #include <envmap_vertex>
       #include <fog_vertex>
     }
@@ -93,6 +93,7 @@ export class ShaderAuroraGUI extends Shader {
     this.uniforms = THREE.UniformsUtils.merge([
       THREE.ShaderLib.basic.uniforms,
       {
+        alphaTest: { value: 1 },
         map: { value: null },
         bounds: { value: new THREE.Vector4(0, 0, 0, 0) },
         scissorBounds: { value: new THREE.Vector4(0, 0, 0, 0) },
