9673812bd5edb17595a682bafc3ada597294c3a2
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2023-02-26 19:34:45 -0500

    add: multicamera support & onClick callbacks

--- src/apps/forge/managers/SceneGraphTreeViewManager.ts
+++ src/apps/forge/managers/SceneGraphTreeViewManager.ts
@@ -7,10 +7,12 @@ export class SceneGraphTreeViewManager {
 
   parentNodes: SceneGraphNode[] = [];
   sceneNode: SceneGraphNode = new SceneGraphNode({name: 'Scene'});
+  camerasNode: SceneGraphNode = new SceneGraphNode({name: 'Cameras'});
   lightingNode: SceneGraphNode = new SceneGraphNode({name: 'Lights'});
   objectsNode: SceneGraphNode = new SceneGraphNode({name: 'Objects'});
 
   constructor(){
+    this.sceneNode.addChildNode(this.camerasNode);
     this.sceneNode.addChildNode(this.lightingNode);
     this.sceneNode.addChildNode(this.objectsNode);
     this.parentNodes.push(this.sceneNode);
@@ -24,16 +26,48 @@ export class SceneGraphTreeViewManager {
 
   rebuild(){
 
+    this.camerasNode.setNodes([]);
     this.lightingNode.setNodes([]);
     this.objectsNode.setNodes([]);
 
     if(this.context){
+
+      if(this.context.camera){
+        this.camerasNode.addChildNode(
+          new SceneGraphNode({
+            uuid: this.context.camera.uuid,
+            name: 'Fly Camera',
+            data: this.context.camera,
+            onClick: (node) => {
+              this.context.currentCamera = node.data;
+            },
+          })
+        );
+      }
+
+      for(let i = 0; i < this.context.cameras.length; i++){
+        const camera = this.context.cameras[i];
+        this.camerasNode.addChildNode(
+          new SceneGraphNode({
+            uuid: camera.uuid,
+            name: camera.name,
+            data: camera,
+            onClick: (node) => {
+              this.context.currentCamera = node.data;
+            },
+          })
+        );
+      }
 
       if(this.context.globalLight){
         this.lightingNode.addChildNode(
           new SceneGraphNode({
             uuid: this.context.globalLight.uuid,
             name: 'Ambient Light',
+            data: this.context.globalLight,
+            onClick: (node) => {
+
+            },
           })
         );
       }
@@ -43,6 +77,10 @@ export class SceneGraphTreeViewManager {
         const modelNode = new SceneGraphNode({
           uuid: model.uuid,
           name: model.name,
+          data: model,
+          onClick: (node) => {
+
+          },
         });
 
         model.nodes.forEach( (node) => {
@@ -50,6 +88,10 @@ export class SceneGraphTreeViewManager {
             new SceneGraphNode({
               uuid: node.uuid,
               name: node.name,
+              data: node,
+              onClick: (node) => {
+                this.context.selectObject(node.data);
+              },
             })
           )
         });
