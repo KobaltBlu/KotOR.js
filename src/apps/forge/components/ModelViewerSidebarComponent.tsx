55c20313c459e290c62c086bf8cc92ccf43afabf
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2023-02-26 19:33:05 -0500

    add: layout and animation features

--- src/apps/forge/components/ModelViewerSidebarComponent.tsx
+++ src/apps/forge/components/ModelViewerSidebarComponent.tsx
@@ -4,17 +4,93 @@ import { useEffectOnce } from "../helpers/UseEffectOnce";
 import { Form } from "react-bootstrap";
 import { SceneGraphTreeView } from "./SceneGraphTreeView";
 
+import * as KotOR from "../KotOR";
+import { ModelViewerControls } from "../ModelViewerControls";
+
 export const ModelViewerSidebarComponent = function(props: any){
   const tab: TabModelViewerState = props.tab as TabModelViewerState;
 
   const [selectedTab, setSelectedTab] = useState<string>('camera');
 
+  const [animations, setAnimations] = useState<KotOR.OdysseyModelAnimation[]>([]);
+  const [selectedAnimation, setSelectedAnimation] = useState<number>(tab.selectedAnimationIndex);
+  const [looping, setLooping] = useState<boolean>(tab.looping);
+
+  const [layouts, setLayouts] = useState<KotOR.KEY[]>([]);
+  const [selectedLayout, setSelectedLayout] = useState<number>(tab.selectedLayoutIndex);
+
+  const [cameraSpeed, setCameraSpeed] = useState<number>(ModelViewerControls.CameraMoveSpeed);
+
+  const onEditorFileLoad = () => {
+    setAnimations(tab.model.odysseyAnimations);
+  }
+
+  const onAnimationChange = function(){
+    setSelectedAnimation(tab.selectedAnimationIndex);
+  };
+
+  const onLoopChange = function(){
+    setLooping(tab.looping);
+  };
+
   useEffectOnce( () => { //constructor
 
-    return () => { //destructor
+    let keys: KotOR.KEY[] = [];
+    let res_list = KotOR.KEYManager.Key.GetFilesByResType(KotOR.ResourceTypes['lyt']);
+    res_list.forEach( (res, index) => {
+      keys.push(
+        KotOR.KEYManager.Key.GetFileKeyByRes(res)
+      );
+    });
+    setLayouts(keys);
 
+    tab.addEventListener('onEditorFileLoad', onEditorFileLoad);
+    tab.addEventListener('onAnimationChange', onAnimationChange);
+    tab.addEventListener('onLoopChange', onLoopChange);
+
+    return () => { //destructor
+      tab.removeEventListener('onEditorFileLoad', onEditorFileLoad);
+      tab.removeEventListener('onAnimationChange', onAnimationChange);
+      tab.removeEventListener('onLoopChange', onLoopChange);
     };
   });
+
+  const onCameraSpeedChange = function(e: React.ChangeEvent<HTMLInputElement>){
+    let value = parseFloat(e.target.value);
+    if(isNaN(value)) value = 10;
+    setCameraSpeed(value);
+    ModelViewerControls.CameraMoveSpeed = value;
+  };
+
+  const onBtnAlignToCameraHook = function(e: React.MouseEvent<HTMLButtonElement>){
+
+  }
+
+  const onAnimationSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = parseInt(e.target.value);
+    setSelectedAnimation(value);
+    tab.setAnimationByIndex(value);
+  };
+
+  const onCheckboxLoopChange = function(e: React.ChangeEvent<HTMLInputElement>){
+    tab.setLooping(e.target.checked);
+    setLooping(e.target.checked);
+  }
+
+  const onLayoutSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = parseInt(e.target.value);
+    tab.selectedLayoutIndex = value;
+    setSelectedLayout(value);
+    // tab.setLayoutByIndex(value);
+  };
+
+  const onBtnLoadLayout = (e: React.MouseEvent<HTMLButtonElement>) => {
+    tab.loadLayout( layouts.find( key => key.ResID == selectedLayout ) );
+  };
+
+  const onBtnDisposeLayout = (e: React.MouseEvent<HTMLButtonElement>) => {
+    tab.disposeLayout();
+  };
 
   return (
     <>
@@ -45,19 +121,24 @@ export const ModelViewerSidebarComponent = function(props: any){
             <div className="toolbar-header">
               <b>Camera Speed</b>
             </div>
-            <input type="range" min="1" max="25" value="10" />
+            <Form.Control type="number" min={1} max={250} value={cameraSpeed} onChange={onCameraSpeedChange} ></Form.Control>
             <div className="button-group">
-              <button>Align to camera hook</button>
+              <button onClick={onBtnAlignToCameraHook}>Align to camera hook</button>
             </div>
           </div>
           <div className="tab-content" style={{display: (selectedTab == 'animation' ? 'block' : 'none')}}>
             <div className="toolbar-header">
               <b>Animations</b>
             </div>
-            <Form.Select>
-              <option value="-1">None</option>
+            <Form.Select defaultValue={-1} value={selectedAnimation} onChange={onAnimationSelectChange}>
+              <option value={-1} selected={selectedAnimation == -1}>None</option>
+              {
+                animations.map( (animation, index) => {
+                  return <option value={index} selected={selectedAnimation == index}>{animation.name}</option>
+                })
+              }
             </Form.Select>
-            <b>Loop? </b><input type="checkbox" />
+            <b>Loop? </b><input type="checkbox" checked={looping} onChange={onCheckboxLoopChange} />
           </div>
           <div className="tab-content" style={{display: (selectedTab == 'nodes' ? 'block' : 'none')}}>
             <div className="toolbar-header">
@@ -83,12 +164,17 @@ export const ModelViewerSidebarComponent = function(props: any){
             <div className="toolbar-header">
               <b>Layout</b>
             </div>
-            <Form.Select>
-              <option value="-1">None</option>
+            <Form.Select defaultValue={-1} value={selectedLayout} onChange={onLayoutSelectChange}>
+              <option value={-1} selected={selectedLayout == -1}>None</option>
+              {
+                layouts.map( (lytKEY) => {
+                  return <option value={lytKEY.ResID} selected={selectedLayout == lytKEY.ResID}>{lytKEY.ResRef}</option>
+                })
+              }
             </Form.Select>
             <div className="button-group">
-              <button>Load</button>
-              <button>Dispose</button>
+              <button onClick={onBtnLoadLayout}>Load</button>
+              <button onClick={onBtnDisposeLayout}>Dispose</button>
             </div>
           </div>
         </div>
