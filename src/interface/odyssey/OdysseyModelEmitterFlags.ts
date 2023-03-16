5ae30036c16ee2a34deb33e975913238686e76c3
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2023-03-16 13:27:36 -0400

    add: emitter flag interface

---
+++ src/interface/odyssey/OdysseyModelEmitterFlags.ts
@@ -0,0 +1,14 @@
+export interface OdysseyModelEmitterFlags {
+  isP2P: boolean;
+  isP2PSel: boolean;
+  affectedByWind: boolean;
+  isTinted: boolean;
+  canBounce: boolean;
+  isRandom: boolean;
+  canInherit: boolean;
+  canInheritVelocity: boolean;
+  canInheritLocal: boolean;
+  canSplat: boolean;
+  canInheritPart: boolean;
+  isDepthTexture: boolean;
+}
