23f55b7fd48c46385280b15abc24132e029c3346
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2023-03-29 23:36:47 -0400

    chore: extend documentation comments

--- src/nwscript/events/EventConversation.ts
+++ src/nwscript/events/EventConversation.ts
@@ -7,6 +7,10 @@ export class EventConversation extends NWScriptEvent {
     super();
     this.type = NWScriptEventType.EventConversation;
 
+    //intList[0]: (?)
+    //intList[1]: (?)
+    //intList[2]: bPrivateConversation
+    //intList[3]: bIgnoreStartRange
     //stringList[*] : strings to speak
 
   }
