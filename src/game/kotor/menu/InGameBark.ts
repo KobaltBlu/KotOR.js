2eedf931814f0ad29e7a0c00a2168e9452ef7ac7
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2023-04-06 16:18:46 -0400

    chore: fix method indent

--- src/game/kotor/menu/InGameBark.ts
+++ src/game/kotor/menu/InGameBark.ts
@@ -34,55 +34,55 @@ export class InGameBark extends GameMenu {
     });
   }
 
-bark(entry: any = null) {
-  if (entry != null) {
-    this.Show();
-    this.LBL_BARKTEXT.setText(entry.text);
-    let size = new THREE.Vector3();
-    this.LBL_BARKTEXT.text.geometry.boundingBox.getSize(size);
-    this.tGuiPanel.extent.height = Math.ceil(size.y) + 14;
-    this.tGuiPanel.resizeControl();
-    this.tGuiPanel.widget.position.x = -window.innerWidth / 2 + this.tGuiPanel.extent.width / 2 + 10;
-    this.tGuiPanel.widget.position.y = window.innerHeight / 2 - this.tGuiPanel.extent.height / 2 - 134;
-    if (entry.sound != '') {
-      console.log('lip', entry.sound);
-      ResourceLoader.loadResource(ResourceTypes['lip'], entry.sound, (buffer: Buffer) => {
-        if (entry.speaker instanceof ModuleCreature) {
-          entry.speaker.setLIP(new LIPObject(buffer));
-        }
-      });
-      MenuManager.InGameDialog.audioEmitter.PlayStreamWave(entry.sound, null, (error = false) => {
-        if (!error) {
-          this.Close();
-        } else {
-          setTimeout(() => {
+  bark(entry: any = null) {
+    if (entry != null) {
+      this.Show();
+      this.LBL_BARKTEXT.setText(entry.text);
+      let size = new THREE.Vector3();
+      this.LBL_BARKTEXT.text.geometry.boundingBox.getSize(size);
+      this.tGuiPanel.extent.height = Math.ceil(size.y) + 14;
+      this.tGuiPanel.resizeControl();
+      this.tGuiPanel.widget.position.x = -window.innerWidth / 2 + this.tGuiPanel.extent.width / 2 + 10;
+      this.tGuiPanel.widget.position.y = window.innerHeight / 2 - this.tGuiPanel.extent.height / 2 - 134;
+      if (entry.sound != '') {
+        console.log('lip', entry.sound);
+        ResourceLoader.loadResource(ResourceTypes['lip'], entry.sound, (buffer: Buffer) => {
+          if (entry.speaker instanceof ModuleCreature) {
+            entry.speaker.setLIP(new LIPObject(buffer));
+          }
+        });
+        MenuManager.InGameDialog.audioEmitter.PlayStreamWave(entry.sound, null, (error = false) => {
+          if (!error) {
             this.Close();
-          }, 3000);
-        }
-      });
-    } else if (entry.vo_resref != '') {
-      console.log('lip', entry.vo_resref);
-      ResourceLoader.loadResource(ResourceTypes['lip'], entry.vo_resref, (buffer: Buffer) => {
-        if (entry.speaker instanceof ModuleCreature) {
-          entry.speaker.setLIP(new LIPObject(buffer));
-        }
-      });
-      MenuManager.InGameDialog.audioEmitter.PlayStreamWave(entry.vo_resref, null, (error = false) => {
-        if (!error) {
-          this.Close();
-        } else {
-          setTimeout(() => {
+          } else {
+            setTimeout(() => {
+              this.Close();
+            }, 3000);
+          }
+        });
+      } else if (entry.vo_resref != '') {
+        console.log('lip', entry.vo_resref);
+        ResourceLoader.loadResource(ResourceTypes['lip'], entry.vo_resref, (buffer: Buffer) => {
+          if (entry.speaker instanceof ModuleCreature) {
+            entry.speaker.setLIP(new LIPObject(buffer));
+          }
+        });
+        MenuManager.InGameDialog.audioEmitter.PlayStreamWave(entry.vo_resref, null, (error = false) => {
+          if (!error) {
             this.Close();
-          }, 3000);
-        }
-      });
-    } else {
-      console.error('VO ERROR', entry);
-      setTimeout(() => {
-        this.Close();
-      }, 3000);
+          } else {
+            setTimeout(() => {
+              this.Close();
+            }, 3000);
+          }
+        });
+      } else {
+        console.error('VO ERROR', entry);
+        setTimeout(() => {
+          this.Close();
+        }, 3000);
+      }
     }
   }
-}
   
 }
