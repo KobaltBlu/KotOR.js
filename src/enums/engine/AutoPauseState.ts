c011d5076ecc7bdea6611ba79533c09090d7e990
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2023-07-04 02:20:12 -0400

    add: AutoPauseManager

---
+++ src/enums/engine/AutoPauseState.ts
@@ -0,0 +1,9 @@
+export enum AutoPauseState {
+  Generic = 0,
+  CombatRoundEnd = 1,
+  EnemySighted = 2,
+  MineSighted = 3,
+  PartyMemberKilled = 4,
+  ActionMenuUsed = 5,
+  NewTargetSelected = 6,
+}
