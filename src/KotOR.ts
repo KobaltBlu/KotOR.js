2ab30217158610954fec3cb81c6c5fef40d1429a
KobaltBlu <10106764+KobaltBlu@users.noreply.github.com>
2023-12-24 16:36:14 -0500

    feat: expose Buffer

--- src/KotOR.ts
+++ src/KotOR.ts
@@ -1,38 +1,56 @@
 export * from "./utility/polyfills";
 export * from "./audio";
-export * from "./odyssey";
 export * from "./module";
 export * from "./gui";
 export * from "./GameInitializer";
 export * from "./GameState";
 export * from "./managers";
-export * from "./utility/ApplicationProfile";
-export * from "./utility/GameFileSystem";
 
 export * from "./BinaryReader";
 export * from "./BinaryWriter";
 
+export * from "./combat";
+
 export * from "./controls/Mouse";
 
+export * from "./enums/ApplicationEnvironment";
+export * from "./enums/ApplicationMode";
 export * from "./enums/engine/GameEngineType";
 export * from "./enums/engine/GameEngineEnv";
 export * from "./enums/engine/EngineMode";
 export * from "./enums/engine/EngineState";
+export * from "./enums/nwscript/NWScriptDataType";
+export * from "./enums/nwscript/NWScriptByteCode";
+export * from "./enums/nwscript/NWScriptDefTypes";
+export * from "./enums/nwscript/NWScriptTypes";
+export * from "./enums/odyssey/OdysseyWalkMeshType";
+export * from "./enums/odyssey/OdysseyModelControllerType";
+export * from "./enums/odyssey/OdysseyModelNodeType";
+export * from "./enums/resource/GFFDataType";
 
 export * from "./interface/resource/LIPHeader";
 export * from "./interface/resource/LIPKeyFrame";
-
-export * from "./combat";
+export * from "./interface/odyssey/OdysseyModelAABBNode";
+export * from "./interface/loaders/TextureLoaderQueuedRef";
 
 export * from "./FactionManager";
 
 export * from "./LoadingScreen";
 export * from "./SaveGame";
 export * from "./utility/ConfigClient";
-export * from "./enums/ApplicationEnvironment";
-export * from "./enums/ApplicationMode";
 
 export * from "./AnimatedTexture";
+
+export * from "./odyssey";
+export * from "./odyssey/controllers/OdysseyController";
+
+export * from "./loaders";
+
+export * from "./nwscript/NWScript";
+export * from "./nwscript/NWScriptInstance";
+export * from "./nwscript/NWScriptInstruction";
+export * from "./nwscript/NWScriptDefK1";
+export * from "./nwscript/NWScriptDefK2";
 
 export * from "./resource/BIFObject";
 export * from "./resource/BIKObject";
@@ -40,7 +58,6 @@ export * from "./resource/ERFObject";
 export * from "./resource/GFFObject";
 export * from "./resource/GFFStruct";
 export * from "./resource/GFFField";
-export * from "./enums/resource/GFFDataType";
 export * from "./resource/KEYObject";
 export * from "./resource/LIPObject";
 export * from "./resource/LTRObject";
@@ -55,28 +72,13 @@ export * from "./resource/VISObject";
 export * from "./resource/ResourceTypes";
 export * from "./resource/ResourceTypeInfo";
 
-export * from "./enums/odyssey/OdysseyWalkMeshType";
-export * from "./interface/odyssey/OdysseyModelAABBNode";
-export * from "./enums/odyssey/OdysseyModelControllerType";
-export * from "./enums/odyssey/OdysseyModelNodeType";
-export * from "./odyssey/controllers/OdysseyController"
-
-export * from "./interface/loaders/TextureLoaderQueuedRef";
-
 export * from "./three/odyssey";
 export * from "./three/MDLLoader";
 
-export * from "./enums/nwscript/NWScriptDataType";
-export * from "./enums/nwscript/NWScriptByteCode";
-export * from "./enums/nwscript/NWScriptDefTypes";
-export * from "./enums/nwscript/NWScriptTypes";
-export * from "./nwscript/NWScript";
-export * from "./nwscript/NWScriptInstance";
-export * from "./nwscript/NWScriptInstruction";
-export * from "./nwscript/NWScriptDefK1";
-export * from "./nwscript/NWScriptDefK2";
-
+export * from "./utility/ApplicationProfile";
+export * from "./utility/GameFileSystem";
 export * from "./utility/Utility";
-export * from "./loaders";
 
 export * as THREE from "three";
+
+export { Buffer } from "buffer";
