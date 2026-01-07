96101fb83baebd60a6439c56407c4308a953a9ca
James <10106764+KobaltBlu@users.noreply.github.com>
2026-01-07 17:50:57 -0500

    Add detailed documentation and initialize properties in CombatAttackData class
    - Introduced comprehensive JSDoc comments for all properties and methods in the CombatAttackData class to enhance code clarity and usability.
    - Initialized previously undefined properties to improve stability and prevent potential runtime errors.
    - Added new methods for calculating damage, applying damage effects, and resetting combat attack data, further enhancing the functionality of the class.

--- src/combat/CombatAttackData.ts
+++ src/combat/CombatAttackData.ts
@@ -22,34 +22,139 @@ import { DiceType } from "../enums/combat/DiceType";
  * @license {@link https://www.gnu.org/licenses/gpl-3.0.txt|GPLv3}
  */
 export class CombatAttackData {
+  /**
+   * The attack group for the attack
+   */
   attackGroup: number = 0;
+
+  /**
+   * The animation length for the attack
+   */
   animationLength: number = 1500;
-  missedBy: ModuleObject;
+
+  /**
+   * The missed by object for the attack
+   */
+  missedBy: ModuleObject = undefined;
+
+  /**
+   * The attack result for the attack
+   */
   attackResult: AttackResult = AttackResult.MISS;
-  reactObject: ModuleObject;
+
+  /**
+   * The reaction object for the attack
+   */
+  reactObject: ModuleObject = undefined;
+
+  /**
+   * The reaction delay for the attack
+   */
   reaxnDelay: number = 0;
+
+  /**
+   * The reaction animation for the attack
+   */
   reaxnAnimation: number = 10001;
+
+  /**
+   * The reaction animation length for the attack
+   */
   reaxnAnimLength: number = 0;
+
+  /**
+   * Whether the attack is concealed
+   */
   concealment: boolean = false;
+
+  /**
+   * Whether the attack is a type attack
+   */
   attackType: boolean = false;
+
+  /**
+   * The attack mode for the attack
+   */
   attackMode: number = 0;
+
+  /**
+   * Whether the attack is a ranged attack
+   */
   rangedAttack: boolean = false;
+
+  /**
+   * Whether the attack is a sneak attack
+   */
   sneakAttack: boolean = false;
+
+  /**
+   * The weapon attack type for the attack
+   */
   weaponAttackType: number = 0;
+
+  /**
+   * The ranged target X coordinate
+   */
   rangedTargetX: number = 0;
+
+  /**
+   * The ranged target Y coordinate
+   */
   rangedTargetY: number = 0;
+
+  /**
+   * The ranged target Z coordinate
+   */
   rangedTargetZ: number = 0;
+
+  /**
+   * The damage list for the attack
+   */
   damageList: CombatAttackDamage[] = new Array(15);
+
+  /**
+   * The killing blow for the attack
+   */
   killingBlow: boolean = false;
+
+  /**
+   * The coup de grace for the attack
+   */
   coupDeGrace: boolean = false;
+
+  /**
+   * The critical threat for the attack
+   */
   criticalThreat: number = 0;
+
+  /**
+   * The attack deflected for the attack
+   */
   attackDeflected: number = 0;
-  ammoItem: ModuleObject;
+
+  /**
+   * The ammo item that is being used for the attack
+   */
+  ammoItem: ModuleObject = undefined;
+
+  /**
+   * The debug text for the attack
+   */
   attackDebugText: CExoLocString;
+
+  /**
+   * The debug text for the damage
+   */
   DamageDebugText: CExoLocString;
 
+  /**
+   * The weapon that is being used for the attack
+   */
   attackWeapon: ModuleItem;
 
+  /**
+   * Constructor for the CombatAttackData class
+   */
   constructor(){
     this.damageList = new Array(15);
     for(let i = 0; i < 15; i++){
@@ -57,6 +162,12 @@ export class CombatAttackData {
     }
   }
 
+  /**
+   * Calculate the damage for the combat attack data
+   * @param creature - The creature to calculate the damage for
+   * @param isCritial - Whether the attack is a critical hit
+   * @param feat - The feat that is being used for the attack
+   */
   calculateDamage(creature: ModuleCreature, isCritial: boolean = false, feat?: TalentFeat){
     /**
      * Unarmed Strike
@@ -121,6 +232,12 @@ export class CombatAttackData {
 
   }
 
+  /**
+   * Calculate the weapon specialization bonus
+   * @param creature - The creature to calculate the weapon specialization bonus for
+   * @param weapon - The weapon to calculate the weapon specialization bonus for
+   * @returns The weapon specialization bonus
+   */
   calculateWeaponSpecBonus(creature: ModuleCreature, weapon: ModuleItem): number {
     let bonus = 0;
     if(!creature){ return; }
@@ -156,6 +273,11 @@ export class CombatAttackData {
     return bonus;
   }
 
+  /**
+   * Apply the damage effect to the target creature
+   * @param owner - The owner of the damage effect
+   * @param target - The target creature to apply the damage effect to
+   */
   applyDamageEffectToCreature(owner: ModuleCreature, target: ModuleCreature){
     if(!target) return;
     const damageEffect = new EffectDamage();
@@ -169,6 +291,10 @@ export class CombatAttackData {
     target.addEffect(damageEffect, GameEffectDurationType.INSTANT);
   }
 
+  /**
+   * Get the total damage of the combat attack data
+   * @returns The total damage
+   */
   getTotalDamage(): number {
     let amount = 0;
     for(let i = 0; i < this.damageList.length; i++){
@@ -180,6 +306,9 @@ export class CombatAttackData {
     return amount;
   }
 
+  /**
+   * Reset the combat attack data
+   */
   reset(){
     this.killingBlow = false;
     this.reactObject = undefined;
@@ -190,6 +319,11 @@ export class CombatAttackData {
     }
   }
 
+  /**
+   * Convert the combat attack data to a GFF struct
+   * @param structIdx - The index of the struct
+   * @returns The GFF struct
+   */
   toStruct(structIdx: number = -1){
     const struct = new GFFStruct(structIdx);
 
